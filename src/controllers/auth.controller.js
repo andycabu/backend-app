74998136cf904924d3d6ebbf19b1aa93752f350e
andycabu <andycabu@gmail.com>
2023-09-21 18:02:10 +0200

    funcion de login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,63 @@
 import User from "../models/user.models.js";
 import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const passwordHash = await bcrypt.hash(password, 10);
+    if (!password) {
+      throw new Error("Se requiere una contraseña");
+    }
+
+    const passwordHash = await bcrypt.hash(password.toString(), 10);
 
     const newUser = new User({ name, email, password: passwordHash });
 
     const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+
     res.json(userSaved);
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
+
 export const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email }).select("+password");
+
+    if (!userFound)
+      return res.status(400).json({ message: "Usuario no encontrado" });
+
+    if (!password)
+      return res.status(400).json({ message: "Se requiere una contraseña" });
+
+    if (!userFound.password)
+      return res
+        .status(400)
+        .json({ message: "La contraseña no está definida" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Contraseña incorrecta" });
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      name: userFound.name,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
