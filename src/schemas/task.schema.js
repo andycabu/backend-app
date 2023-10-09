e54ca2071ddd22ba0f4021caf6dadb3b2b6222c2
andycabu <andycabu@gmail.com>
2023-10-09 19:48:25 +0200

    modified:   src/schemas/task.schema.js

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -1,11 +1,19 @@
 import { z } from "zod";
 
 export const taskSchema = z.object({
-  title: z.string({
-    required_error: "Se requiere un título",
-  }),
-  description: z.string({
-    required_error: "Se requiere una descripción",
-  }),
+  title: z
+    .string({
+      required_error: "Se requiere un título",
+    })
+    .min(4, {
+      message: "El titulo debe tener minimo cuatro caracteres",
+    }),
+  description: z
+    .string({
+      required_error: "Se requiere una descripción",
+    })
+    .min(4, {
+      message: "La descripcion debe tener minimo cuatro caracteres",
+    }),
   date: z.string().datetime().optional(),
 });
